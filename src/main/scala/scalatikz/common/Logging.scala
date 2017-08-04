36b1e29a6524f066c33bf466d456bd3797fba8bd
Vagelis Michelioudakis <vagmcs@gmail.com>
2017-08-04 14:47:09 +0300

    Missing headers added.

--- src/main/scala/scalatikz/common/Logging.scala
+++ src/main/scala/scalatikz/common/Logging.scala
@@ -1,3 +1,29 @@
+/*
+ *    ____         __    ____________ ______
+ *   / __/______ _/ /__ /_  __/  _/ //_/_  /
+ *  _\ \/ __/ _ `/ / _ `// / _/ // ,<   / /_
+ * /___/\__/\_,_/_/\_,_//_/ /___/_/|_| /___/
+ *
+ * ScalaTIKZ.
+ *
+ * Copyright (c) Evangelos Michelioudakis.
+ *
+ * This file is part of ScalaTIKZ.
+ *
+ * ScalaTIKZ is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * ScalaTIKZ is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ScalaTIKZ. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package scalatikz.common
 
 import com.typesafe.scalalogging.LazyLogging
