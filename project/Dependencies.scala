3eaa0bcf309089a6a87b2faa57e33572c1da2d9c
Evangelos Michelioudakis <vagmcs@gmail.com>
2019-01-09 11:13:49 +0200

    dependencies updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,32 +15,32 @@ object Dependencies {
 
   object v {
     final val Logback = "1.2.3"
-    final val ScalaLogging = "3.7.2"
+    final val ScalaLogging = "3.9.0"
 
-    final val ScalaTest = "3.0.1"
+    final val ScalaTest = "3.0.5"
     final val ScalaCheck = "1.13.4"
 
-    final val ScalaOpt = "3.7.0"
-    final val Ghost4J = "1.0.0"
-    final val CSVParser = "2.7.2"
+    final val ScalaOpt = "3.7.1"
+    final val Ghost4J = "1.0.1"
+    final val CSVParser = "2.7.6"
     final val NameOf = "1.0.3"
     final val Enums = "1.5.13"
   }
 
   // Logging using slf4j and logback
-  lazy val Logging = Seq(
+  lazy val Logging: Seq[ModuleID] = Seq(
     "ch.qos.logback" % "logback-classic" % v.Logback,
     "com.typesafe.scala-logging" %% "scala-logging" % v.ScalaLogging
   )
 
   // ScalaTest and ScalaMeter for UNIT testing
-  lazy val ScalaTest = Seq(
+  lazy val ScalaTest: Seq[ModuleID] = Seq(
     "org.scalatest" %% "scalatest" % v.ScalaTest % "test",
     "org.scalacheck" %% "scalacheck" % v.ScalaCheck % "test"
   )
 
   // Libraries for option parsing, csv parsing and ghost-script
-  lazy val Commons = Seq(
+  lazy val Commons: Seq[ModuleID] = Seq(
     "com.github.scopt" %% "scopt" % v.ScalaOpt,
     "org.ghost4j" % "ghost4j" % v.Ghost4J,
     "com.univocity" % "univocity-parsers" % v.CSVParser,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.6")
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.15")
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.3")
 
@@ -6,7 +6,7 @@ addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.1.1")
 
 addSbtPlugin("com.scalapenos" % "sbt-prompt" % "1.0.2")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.9")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.11")
 
 addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.0.0")
 
