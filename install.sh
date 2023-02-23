deb69585084e6d132a5a681e05377e4252797859
Evangelos Michelioudakis <vagmcs@gmail.com>
2023-02-23 21:00:32 +0200

    build: add installation script

---
+++ install.sh
@@ -0,0 +1,6 @@
+LATEST_VERSION=0.5.1
+wget -q --show-progress -P /tmp https://github.com/vagmcs/ScalaTIKZ/releases/download/v"${LATEST_VERSION}"/ScalaTIKZ-"${LATEST_VERSION}".zip
+unzip -oq -d /tmp /tmp/ScalaTIKZ-"${LATEST_VERSION}".zip && rm -r /tmp/ScalaTIKZ-"${LATEST_VERSION}".zip
+cp -r /tmp/ScalaTIKZ-"${LATEST_VERSION}" "${HOME}"/.local/opt && rm -r /tmp/ScalaTIKZ-"${LATEST_VERSION}"
+ln -sfn "${HOME}"/.local/opt/ScalaTIKZ-"${LATEST_VERSION}" "${HOME}"/.local/opt/scalatikz
+echo "Add 'export PATH=${HOME}/.local/opt/scalatikz/bin:\${PATH}' to your $(basename "${SHELL}")"
